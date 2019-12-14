6e98a6c7a60b54c3e870539af55eeef0ff01a5ca
WenchengLi-WL <babystar6666@outlook.com>
2019-12-14 11:58:26 +0900

    Creating our Cart Modal Page

--- src/app/pages/cart-modal/cart-modal.page.ts
+++ src/app/pages/cart-modal/cart-modal.page.ts
@@ -1,15 +1,62 @@
+import { Product, CartService } from './../../services/cart.service';
 import { Component, OnInit } from '@angular/core';
+import { ModalController, AlertController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-cart-modal',
-  templateUrl: './cart-modal.page.html',
-  styleUrls: ['./cart-modal.page.scss'],
+    selector: 'app-cart-modal',
+    templateUrl: './cart-modal.page.html',
+    styleUrls: ['./cart-modal.page.scss'],
 })
 export class CartModalPage implements OnInit {
+    cart: Product[] = [];
 
-  constructor() { }
+    constructor(
+        private cartService: CartService,
+        private modalCtrl: ModalController,
+        private alertCtrl: AlertController
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.cart = this.cartService.getCart();
+    }
+
+    decreaseCartItem(product)
+    {
+        this.cartService.decreaseProduct(product);
+    }
+
+    increaseCartItem(product)
+    {
+        this.cartService.addProduct(product);
+    }
+
+    removeCartItem(product)
+    {
+        this.cartService.removeProduct(product);
+    }
+
+    getTotal()
+    {
+        return this.cart.reduce((i, j) => i + j.price * j.price, 0);
+    }
+
+    close()
+    {
+        this.modalCtrl.dismiss();
+    }
+
+    async checkout()
+    {
+        // perform Paypal or Stripe checkout process
+
+        let alert = await this.alertCtrl.create({
+            header: 'Thanks for your order!',
+            message: 'We will deliver your food as soon as possible',
+            buttons: ['OK']
+        });
+        alert.present().then(() => {
+            this.modalCtrl.dismiss();
+        });
+    }
 
 }
